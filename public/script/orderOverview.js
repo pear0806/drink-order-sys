b682e25524eea42a8f7d27396bfd267a1d9481a7
Pear <a0908512313@gmail.com>
2025-05-18 18:40:54 +0800

    init commit

---
+++ public/script/orderOverview.js
@@ -0,0 +1,22 @@
+/*
+<div class="order">
+<div class="imformationDiv">
+<h3>店家 :</h3>
+<p>Macu</p>
+</div>
+<div class="imformationDiv">
+<h3>發起人 :</h3>
+<p>Pear</p>
+</div>
+
+<div class="imformationDiv">
+<h3>訂單ID :</h3>
+<p class="id">1747563608904</p>
+</div>
+
+<div class="imformationDiv">
+<h3>備註 :</h3>
+<p>for all people</p>
+</div>
+</div>
+*/
